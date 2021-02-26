d777da5c4d8e51a1c16634e0ec014b8a493a48b8
Michael Caley <mjcaley@darkarctic.com>
2021-02-25 23:36:01 -0500

    Restructured the language to make parsing easier

--- tests/test_assembler_parser.py
+++ tests/test_assembler_parser.py
@@ -5,15 +5,15 @@ def test_parses_start(parser):
     result = parser("start").parse(
         """
         struct String
-            u64
+            u64,
             addr
     
-        define FortyTwo = 42 i32
-        define Error = "Error occurred" str
+        define FortyTwo i32 = 42
+        define Error str = "Error occurred"
         
         func add locals=2, args=2
-            ldlocal 0
-            ldlocal 1
+            ldlocal i32 0
+            ldlocal i32 1
             add
             ret
     """
@@ -29,77 +29,45 @@ def test_parses_struct(parser):
     result = parser("struct").parse(
         """
         struct Object
-            i32
-            u8
-            addr
+            i32, u8, addr
     """
     )
 
     assert "Object" == result.children[0].value
-    assert "types" == result.children[1].data
-
-
-def test_parses_types(parser):
-    result = parser("types").parse(
-        """
-        i8
-        i16
-        i32
-        i64
-        u8
-        u16
-        u32
-        u64
-        f32
-        f64
-        str
-        addr
-    """
-    )
-
-    assert "i8" == result.children[0].value
-    assert "i16" == result.children[1].value
-    assert "i32" == result.children[2].value
-    assert "i64" == result.children[3].value
-    assert "u8" == result.children[4].value
-    assert "u16" == result.children[5].value
-    assert "u32" == result.children[6].value
-    assert "u64" == result.children[7].value
-    assert "f32" == result.children[8].value
-    assert "f64" == result.children[9].value
-    assert "str" == result.children[10].value
-    assert "addr" == result.children[11].value
+    assert "I32" == result.children[1].type
+    assert "U8" == result.children[2].type
+    assert "ADDR" == result.children[3].type
 
 
 def test_parses_definition(parser):
-    result = parser("definition").parse("define test = 42 i32")
+    result = parser("definition").parse("define test i32 = 42")
 
     assert "test" == result.children[0].value
-    assert "int_operand" == result.children[1].data
+    assert "42" == result.children[2].children[0].value
 
 
 def test_parses_function(parser):
     result = parser("function").parse(
         """
         func add locals=2, args=2
-            ldlocal 0
-            ldlocal 1
+            ldlocal i32 0
+            ldlocal i32 1
             add
             ret
     """
     )
 
     assert "add" == result.children[0].value
-    assert "2" == result.children[1].value
-    assert "2" == result.children[2].value
+    assert "2" == result.children[1].children[0].value
+    assert "2" == result.children[2].children[0].value
     assert "statements" == result.children[3].data
 
 
 @pytest.mark.parametrize(
     "test_input,expected",
     [
-        ("noop", "nullary_instruction"),
-        ("ldlocal 42 i32", "unary_instruction"),
+        ("noop", "instruction"),
+        ("ldlocal i32 42", "instruction"),
         ("label_name:", "label"),
     ],
 )
@@ -131,8 +99,8 @@ def test_parses_statement(parser, test_input, expected):
         ("ret", "RET"),
     ],
 )
-def test_parses_nullary_instruction(parser, test_input, expected):
-    result = parser("nullary_instruction").parse(test_input)
+def test_parses_nullary_instruction_without_type(parser, test_input, expected):
+    result = parser("instruction").parse(test_input)
 
     assert expected == result.children[0].type
 
@@ -140,23 +108,53 @@ def test_parses_nullary_instruction(parser, test_input, expected):
 @pytest.mark.parametrize(
     "test_input,expected",
     [
-        ("ldconst 42", "LDCONST"),
-        ("ldlocal 42", "LDLOCAL"),
-        ("stlocal 42", "STLOCAL"),
-        ("callfunc 42", "CALLFUNC"),
-        ("newstruct 42", "NEWSTRUCT"),
-        ("ldfield 42", "LDFIELD"),
-        ("stfield 42", "STFIELD"),
-        ("newarray 42", "NEWARRAY"),
-        ("ldelem 42", "LDELEM"),
-        ("stelem 42", "STELEM"),
+        ("halt void", "HALT"),
+        ("noop void", "NOOP"),
+        ("add void", "ADD"),
+        ("sub void", "SUB"),
+        ("mul void", "MUL"),
+        ("div void", "DIV"),
+        ("mod void", "MOD"),
+        ("pop void", "POP"),
+        ("jmp void", "JMP"),
+        ("jmpt void", "JMPT"),
+        ("jmpf void", "JMPF"),
+        ("testeq void", "TESTEQ"),
+        ("testne void", "TESTNE"),
+        ("testgt void", "TESTGT"),
+        ("testlt void", "TESTLT"),
+        ("callvirt void", "CALLVIRT"),
+        ("ret void", "RET"),
+    ],
+)
+def test_parses_nullary_instruction_with_type(parser, test_input, expected):
+    result = parser("instruction").parse(test_input)
+
+    assert expected == result.children[0].type
+    assert "VOID" == result.children[1].type
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("ldconst i32 42", "LDCONST"),
+        ("ldlocal i32 42", "LDLOCAL"),
+        ("stlocal i32 42", "STLOCAL"),
+        ("callfunc i32 42", "CALLFUNC"),
+        ("newstruct i32 42", "NEWSTRUCT"),
+        ("ldfield i32 42", "LDFIELD"),
+        ("stfield i32 42", "STFIELD"),
+        ("newarray i32 42", "NEWARRAY"),
+        ("ldelem i32 42", "LDELEM"),
+        ("stelem i32 42", "STELEM"),
     ],
 )
 def test_parses_unary_instruction(parser, test_input, expected):
-    result = parser("unary_instruction").parse(test_input)
+    result = parser("instruction").parse(test_input)
 
     assert expected == result.children[0].type
-    assert "int_operand" == result.children[1].data
+    assert "I32" == result.children[1].type
+    assert "42" == result.children[2].children[0].value
 
 
 def test_parses_label(parser):
@@ -166,71 +164,36 @@ def test_parses_label(parser):
     assert "CNAME" == result.children[0].type
 
 
-# @pytest.mark.parametrize("test_input,expected", [
-#     ("42 i32", "int_operand"),
-#     ("42.0 f32", "float_operand"),
-#     ('"A string" str', "str_operand"),
-#     ("identifier", "binding"),
-# ])
-# def test_operand_returns_sub_rule(parser, test_input, expected):
-#     result = parser("_operand").parse(test_input)
-#
-#     assert expected == result.data
+def test_parses_dec_integer(parser):
+    result = parser("dec_integer").parse("42")
 
-
-@pytest.mark.parametrize(
-    "test_input,expected",
-    [
-        ("i8", "I8"),
-        ("i16", "I16"),
-        ("i32", "I32"),
-        ("i64", "I64"),
-        ("u8", "U8"),
-        ("u16", "U16"),
-        ("u32", "U32"),
-        ("u64", "U64"),
-    ],
-)
-def test_parses_int_operand(parser, test_input, expected):
-    result = parser("int_operand").parse(f"42 {test_input}")
-
-    assert "SIGNED_INT" == result.children[0].type
-    assert "42" == result.children[0].value
-    assert expected == result.children[1].type
-
-
-def test_parses_int_operand_without_type(parser):
-    result = parser("int_operand").parse("42")
-
-    assert "SIGNED_INT" == result.children[0].type
+    assert "INT" == result.children[0].type
     assert "42" == result.children[0].value
 
 
-@pytest.mark.parametrize(
-    "test_input,expected",
-    [
-        ("f32", "F32"),
-        ("f64", "F64"),
-    ],
-)
-def test_parses_float_operand(parser, test_input, expected):
-    result = parser("float_operand").parse(f"42.0 {test_input}")
+def test_parses_hex_integer(parser):
+    result = parser("hex_integer").parse("0x42")
 
-    assert "SIGNED_FLOAT" == result.children[0].type
+    assert "HEXADECIMAL" == result.children[0].type
+    assert "42" == result.children[0].value
+
+
+def test_parses_bin_integer(parser):
+    result = parser("bin_integer").parse("0b1010")
+
+    assert "BINARY" == result.children[0].type
+    assert "1010" == result.children[0].value
+
+
+def test_parses_float_literal(parser):
+    result = parser("float_literal").parse(f"42.0")
+
+    assert "DECIMAL" == result.children[0].type
     assert "42.0" == result.children[0].value
-    assert expected == result.children[1].type
 
 
-def test_parses_string_operand(parser):
-    result = parser("str_operand").parse('"String value" str')
-
-    assert "ESCAPED_STRING" == result.children[0].type
-    assert '"String value"' == result.children[0].value
-    assert "STR" == result.children[1].type
-
-
-def test_parses_string_operand_without_type(parser):
-    result = parser("str_operand").parse('"String value"')
+def test_parses_string_literal(parser):
+    result = parser("string_literal").parse('"String value"')
 
     assert "ESCAPED_STRING" == result.children[0].type
     assert '"String value"' == result.children[0].value
